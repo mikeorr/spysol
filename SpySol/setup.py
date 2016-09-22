515c741977c9f065901d18bed4d5eba1124e06eb
Mike Orr <sluggoster@gmail.com>
2016-09-21 23:06:45 -0700

    Initial SpySol distribution.

---
+++ SpySol/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+def readme():
+    with open("README.rst") as f:
+        return f.read()
+
+REQUIRES  =  [
+    ]
+
+setuptools.setup(
+    name = "SpySol",
+    version = "0.0",
+    description = "A Spider solitaire game.",
+    #long_description = readme()
+    classifiers = [
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Natural Language :: Esperanto",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Games/Entertainment :: Puzzle Games",
+        ],
+    keywords = "spider solitaire card game",
+    url = "https://github.com/mikeorr/spysol",
+    author = "Mike Orr",
+    author_email = "sluggoster@gmail.com",
+    license = "MIT",
+    packages = ["spysol"],
+    install_requires = REQUIRES,
+    #tests_require = ["pytest"],
+    #setup_requires = ["pytest-runner"],
+    extras_require = {
+        "tests": ["pytest"],
+        },
+    zip_safe = False,
+    )
